8334b0d92a0496863f47cb4426225e9c0ae01af9
EulerSolutionsMatt <47004936+EulerSolutionsMatt@users.noreply.github.com>
2021-04-26 07:11:50 +0100

    Add base controller functionality - not working properly yet

--- public/index.php
+++ public/index.php
@@ -1,13 +1,14 @@
 <?php
 
-require_once __DIR__ . '../../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use app\core\Application;
+use app\controllers\SiteController;
 
 
 $app = new Application(dirname(__DIR__));
 
-$app->router->get('/', 'home');
+$app->router->get('/', [SiteController::class, 'home']);
 $app->router->get('/contact', 'contact');
 $app->router->post('/contact', function () {
     return "Submitting form " . print_r($_POST, true);
